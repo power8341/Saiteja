7e406d1777fd2b0f84bee8bb31214fcf7bf5f689
saiteja <esaiteja014@gmail.com>
2022-12-16 08:58:12 +0530

    src/index.js

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,31 +1,13 @@
 const mongoose = require('mongoose');
+//Create a bookSchema with bookName, authorName, category and year . 
+//Create same 2 api's for books i.e. : 1 api to create a 
+//new book and another api to get the list of all books. 
 
-const userSchema = new mongoose.Schema( {
-    firstName: String,
-    lastName: String,
-    mobile: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    emailId: String,
-    gender: {
-        type: String,
-        enum: ["male", "female", "LGBTQ"] //"falana" will give an error
-    },
-    age: Number,
-    // isIndian: Boolean,
-    // parentsInfo: {
-    //     motherName: String,
-    //     fatherName: String,
-    //     siblingName: String
-    // },
-    // cars: [ String  ]
+const bookSchema = new mongoose.Schema( {
+    bookName: {type:String,required:true},
+    authorName: {type:String,required:true},
+    category:{type:String,required:true},
+    year:{type:Number,required:true}
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema) //users
-
-
-
-// String, Number
-// Boolean, Object/json, array
+module.exports = mongoose.model('User', bookSchema) //users
